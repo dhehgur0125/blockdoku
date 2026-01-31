57750226316ab6fa1ee6b2294968f5ffd35537c6
dhehgur0125 <dhehgur0125@naver.com>
2026-01-31 19:21:11 +0900

    ui 추가

---
+++ app/src/main/java/com/example/blockdoku/BombTutorialActivity.kt
@@ -0,0 +1,20 @@
+package com.example.blockdoku
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BombTutorialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bomb_tutorial)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bomb_tutorial)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
